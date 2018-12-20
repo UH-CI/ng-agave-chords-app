92041bd0f494968f24bd26cd98d2577af582f699
Sean B. Cleveland <sean.b.cleveland@gmail.com>
2018-12-20 14:21:54 -0700

    update spatial to use agave chords api

--- src/app/_services/spatial.service.ts
+++ src/app/_services/spatial.service.ts
@@ -1,8 +1,9 @@
 import { Injectable } from '@angular/core';
 import { Metadata } from '../_models/metadata';
 import { User } from '../_models/user';
-import { Observable, of } from 'rxjs';
-//import { MessageService } from './message.service';
+import { Observable} from 'rxjs';
+import { throwError } from 'rxjs';
+import { MessageService } from './message.service';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { map, retry, catchError } from 'rxjs/operators';
 import { AppConfig } from './config.service';
@@ -21,10 +22,9 @@ export class SpatialService {
     interface ResponseResults {
      result: any
     }
-     let query = "{'$and':[{'value.loc': {$geoWithin: {'$geometry':"+JSON.stringify(geometry).replace(/"/g,'\'')+"}}}]}";
-     console.log(query)
-     let url = AppConfig.settings.aad.tenant+"/meta/v2/data?q="+encodeURI(query)+"&limit=1000&offset=0";
-        //.set("Authorization", "Bearer " + currentUser.access_token)
+     let query = "geometry="+JSON.stringify(geometry).replace(/"/g,'\'');
+     let url = AppConfig.settings.apiServer.agaveChords+"/spatial?"+encodeURI(query);
+    console.log(url)
      let head = new HttpHeaders()
      .set("Content-Type", "application/x-www-form-urlencoded");
      let options = {
@@ -40,9 +40,10 @@ export class SpatialService {
         console.log("more")
         return data.result as Metadata[];
       }),
-      catchError((e) => {
-        return Observable.throw(new Error(e.message));
-      }),
+      catchError((error: any) => {
+        console.log(error)
+        return throwError(error.statusText);
+    }),
     );
     return response;
    }
